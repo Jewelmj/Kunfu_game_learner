fix: resolve rollout buffer overflow

config.py
@@ -15,7 +15,7 @@ DEFAULT_ACTION = 3
 
 # Agent selection
 AGENT_TYPE = 'DQN'    # Options: 'DQN', 'PPO', 'LINEAR'
-N_ENVS = 1            # Number of parallel environments
+N_ENVS = 10            # Number of parallel environments
 
 # --- Epsilon-Greedy ---
 EPSILON_START = 1.0
@@ -27,20 +27,20 @@ BUFFER_SIZE = 50000
 BATCH_SIZE = 100
 GAMMA = 0.99
 LEARNING_RATE = 0.0000625
-TARGET_UPDATE_FREQ = 10000
+TARGET_UPDATE_FREQ = 5_000
 TRAINING_STARTS = 1000
-TOTAL_TIMESTEPS = 1_000_000
+TOTAL_TIMESTEPS = 100_000
 
 NUM_FRAMES = 4  # Stacked frames for temporal context
 CONV_OUT_CHANNELS = [32, 64, 64]
 CONV_KERNELS = [8, 4, 3]
-CONV_STRIDEs = [4, 2, 1]
+CONV_STRIDES = [4, 2, 1]
 FC_UNITS1 = 512
 FC_UNITS2 = 512
 CONV_OUT_SIZE = CONV_OUT_CHANNELS[2] * (
-    (((((FRAME_HEIGHT - CONV_KERNELS[0]) // CONV_STRIDEs[0] + 1)
-       - CONV_KERNELS[1]) // CONV_STRIDEs[1] + 1
-       - CONV_KERNELS[2]) // CONV_STRIDEs[2] + 1) ** 2
+    (((((FRAME_HEIGHT - CONV_KERNELS[0]) // CONV_STRIDES[0] + 1)
+       - CONV_KERNELS[1]) // CONV_STRIDES[1] + 1
+       - CONV_KERNELS[2]) // CONV_STRIDES[2] + 1) ** 2
 )
 
 # Onpolicy hyper parameters
@@ -56,18 +56,18 @@ PPO_GAE_LAMBDA = 0.95
 
 # Log file
 LOG_FOLDER = "results"
-LOG_FILE = f"{LOG_FOLDER}/log/training_log.csv"
-LOG_EVERY_N_STEPS = 1000
+LOG_FILE = f"{LOG_FOLDER}/log/training_log_{AGENT_TYPE}.csv"
+LOG_EVERY_N_STEPS = 300
 LOG_CURRENT_BEST_MODEL_AS = f"{LOG_FOLDER}/saved_models/best_kungfu_{AGENT_TYPE}_"
 LOG_FINAL_BEST_MODEL_AS = f"{LOG_FOLDER}/saved_models/kungfu_{AGENT_TYPE}_final"
 
 # video file location
 MODEL_NAME_FOR_VIDEO = f"saved_models/kungfu_{AGENT_TYPE}_final"
-OUTPUT_VIDEO_FILE_NAME = "video/evaluation_video"
+OUTPUT_VIDEO_FILE_NAME = f"video/evaluation_video{AGENT_TYPE}"
 
 # plot log files
-LOG_FILES_TO_PLOT = ["results/log/training_log.csv",
-                    "results/log/training_log2.csv"]
+LOG_FILES_TO_PLOT = [f"results/log/training_log_{AGENT_TYPE}.csv",
+                    f"results/log/training_log_{AGENT_TYPE}.csv"]
 
 # initialise device
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")

memory/rollout_buffer.py
@@ -9,14 +9,14 @@ class RolloutBuffer:
     It clears after every major learning step.
     """
     def __init__(self, buffer_size, state_shape):
-        self.states = np.empty((buffer_size, *state_shape), dtype=np.float32)
-        self.actions = np.empty(buffer_size, dtype=np.int64)
-        self.rewards = np.empty(buffer_size, dtype=np.float32)
+        self.states = np.empty((buffer_size, *state_shape), dtype=np.uint8)
+        self.actions = np.empty(buffer_size, dtype=np.int16)
+        self.rewards = np.empty(buffer_size, dtype=np.float16)
         self.dones = np.empty(buffer_size, dtype=np.bool_)
-        self.log_probs = np.empty(buffer_size, dtype=np.float32)
-        self.values = np.empty(buffer_size, dtype=np.float32)
-        self.returns = np.empty(buffer_size, dtype=np.float32)
-        self.advantages = np.empty(buffer_size, dtype=np.float32)
+        self.log_probs = np.empty(buffer_size, dtype=np.float16)
+        self.values = np.empty(buffer_size, dtype=np.float16)
+        self.returns = np.empty(buffer_size, dtype=np.float16)
+        self.advantages = np.empty(buffer_size, dtype=np.float16)
         self.ptr = 0
         self.max_size = buffer_size
 
@@ -24,13 +24,14 @@ class RolloutBuffer:
         if self.ptr >= self.max_size:
             raise IndexError("Rollout buffer exceeded max capacity.")
             
-        self.states[self.ptr] = state
-        self.actions[self.ptr] = action
-        self.rewards[self.ptr] = reward
-        self.dones[self.ptr] = done
-        self.log_probs[self.ptr] = log_prob
-        self.values[self.ptr] = value
-        self.ptr += 1
+        idx = self.ptr % self.max_size  # circular indexing
+        self.states[idx] = state
+        self.actions[idx] = action
+        self.rewards[idx] = reward
+        self.dones[idx] = done
+        self.log_probs[idx] = log_prob
+        self.values[idx] = value
+        self.ptr = (self.ptr + 1) % self.max_size
 
     def clear(self):
         self.ptr = 0

models/cnn_policy_network.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from config import NUM_FRAMES, FC_UNITS1, FC_UNITS2, CONV_OUT_CHANNELS, CONV_KERNELS, CONV_STRIDEs, CONV_OUT_SIZE
+from config import NUM_FRAMES, FC_UNITS1, FC_UNITS2, CONV_OUT_CHANNELS, CONV_KERNELS, CONV_STRIDES, CONV_OUT_SIZE
 
 class ActorCriticNetwork(nn.Module):
     def __init__(self, action_size):
@@ -13,9 +13,9 @@ class ActorCriticNetwork(nn.Module):
         super(ActorCriticNetwork, self).__init__()
 
         # --- Shared CNN Backbone (Same as DQN QNetwork) ---
-        self.conv1 = nn.Conv2d(in_channels=NUM_FRAMES, out_channels=CONV_OUT_CHANNELS[0], kernel_size=CONV_KERNELS[0], stride=CONV_STRIDEs[0])
-        self.conv2 = nn.Conv2d(in_channels=CONV_OUT_CHANNELS[0], out_channels=CONV_OUT_CHANNELS[1], kernel_size=CONV_KERNELS[1], stride=CONV_STRIDEs[1])
-        self.conv3 = nn.Conv2d(in_channels=CONV_OUT_CHANNELS[1], out_channels=CONV_OUT_CHANNELS[2], kernel_size=CONV_KERNELS[2], stride=CONV_STRIDEs[2]) 
+        self.conv1 = nn.Conv2d(in_channels=NUM_FRAMES, out_channels=CONV_OUT_CHANNELS[0], kernel_size=CONV_KERNELS[0], stride=CONV_STRIDES[0])
+        self.conv2 = nn.Conv2d(in_channels=CONV_OUT_CHANNELS[0], out_channels=CONV_OUT_CHANNELS[1], kernel_size=CONV_KERNELS[1], stride=CONV_STRIDES[1])
+        self.conv3 = nn.Conv2d(in_channels=CONV_OUT_CHANNELS[1], out_channels=CONV_OUT_CHANNELS[2], kernel_size=CONV_KERNELS[2], stride=CONV_STRIDES[2]) 
         
         # --- Actor (Policy) Head ---
         self.actor_fc1 = nn.Linear(CONV_OUT_SIZE, FC_UNITS1)

models/cnn_q_network.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from config import NUM_FRAMES, CONV_OUT_CHANNELS, CONV_KERNELS, CONV_STRIDEs, FC_UNITS1, FC_UNITS2, CONV_OUT_SIZE
+from config import NUM_FRAMES, CONV_OUT_CHANNELS, CONV_KERNELS, CONV_STRIDES, FC_UNITS1, FC_UNITS2, CONV_OUT_SIZE
 
 class QNetwork(nn.Module):
     def __init__(self, action_size):
@@ -13,9 +13,9 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
 
         # 1. Input: (NUM_FRAMES, 84, 84) -> Output: (32, 20, 20)
-        self.conv1 = nn.Conv2d(in_channels=NUM_FRAMES, out_channels=CONV_OUT_CHANNELS[0], kernel_size=CONV_KERNELS[0], stride=CONV_STRIDEs[0])
-        self.conv2 = nn.Conv2d(in_channels=CONV_OUT_CHANNELS[0], out_channels=CONV_OUT_CHANNELS[1], kernel_size=CONV_KERNELS[1], stride=CONV_STRIDEs[1])
-        self.conv3 = nn.Conv2d(in_channels=CONV_OUT_CHANNELS[1], out_channels=CONV_OUT_CHANNELS[2], kernel_size=CONV_KERNELS[2], stride=CONV_STRIDEs[2]) # 64, 9, 9) -> (64, 7, 7)
+        self.conv1 = nn.Conv2d(in_channels=NUM_FRAMES, out_channels=CONV_OUT_CHANNELS[0], kernel_size=CONV_KERNELS[0], stride=CONV_STRIDES[0])
+        self.conv2 = nn.Conv2d(in_channels=CONV_OUT_CHANNELS[0], out_channels=CONV_OUT_CHANNELS[1], kernel_size=CONV_KERNELS[1], stride=CONV_STRIDES[1])
+        self.conv3 = nn.Conv2d(in_channels=CONV_OUT_CHANNELS[1], out_channels=CONV_OUT_CHANNELS[2], kernel_size=CONV_KERNELS[2], stride=CONV_STRIDES[2]) # 64, 9, 9) -> (64, 7, 7)
         
         # 4. Input: 3136 -> Output: 512
         self.fc4 = nn.Linear(CONV_OUT_SIZE, FC_UNITS1)